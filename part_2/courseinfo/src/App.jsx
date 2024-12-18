f5eaf0deba29eda5deee4aa9ee332e33af248ad6
eonryan168 <ryanalexanderhartono@gmail.com>
2024-12-18 12:42:04 +0800

    completed 2.2, 2.3

--- part_2/courseinfo/src/App.jsx
+++ part_2/courseinfo/src/App.jsx
@@ -7,13 +7,15 @@ const Part = ({ part }) =>
     {part.name} {part.exercises}
   </p>
 
-const Content = ({ parts }) => {return parts.map((part) => <Part key={part.id} part={part} />)}
+const Content = ({ parts }) => parts.map((part) => <Part key={part.id} part={part} />);
 
 const Course = ({course}) => {
+  const total = course.parts.reduce((total, currentValue) => total + currentValue.exercises, 0)
   return (
   <>
   <Header course={course.name} />
   <Content parts={course.parts} />
+  <Total sum={total} />
   </>
   )
 }
