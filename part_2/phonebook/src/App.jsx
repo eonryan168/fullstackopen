e88656c6a9c69d9143b777af454cd7619214d52a
eonryan168 <ryanalexanderhartono@gmail.com>
2024-12-19 23:07:59 +0800

    completed 2.8

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    // { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [number, setNumber] = useState('')
 
   const handleNameChange = (event) => {
     // console.log(event.target)
     setNewName(event.target.value)
+  }
+
+  const handleNumber = (event) => {
+    // console.log(event.target)
+    setNumber(event.target.value)
   }
 
   const checkDuplicate = () => {
@@ -21,37 +27,42 @@ const App = () => {
     return duplicateTrue;
   };
 
-  const addName = (event) => {
+  const addDetails = (event) => { 
     event.preventDefault()
     // console.log(event.target)
     // console.log(`newName ${newName}`)
-    const nameToAdd = {name: newName}
-    // console.log(nameToAdd)
+    const detailsToAdd = {name: newName, number: number}
+    console.log(detailsToAdd)
+    // console.log(persons)
 
     if (checkDuplicate() === true) {
       alert(`${newName} is already added to the phonebook`)
       setNewName("")
     } else {
-      setPersons(persons.concat(nameToAdd)) //because of async nature of setPersons, 
-                                          //console.log(persons) show the state before the
+      setPersons(persons.concat(detailsToAdd)) //because of async nature of setPersons, 
+                                              //console.log(persons) show the state before the
       setNewName("")
+      setNumber("")
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addDetails}> 
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input type='tel' value={number} onChange={handleNumber}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name} style={{ margin: 0, padding: 0 }}>{person.name}</p>)}
-      <div>debug: {newName}</div>
+      {persons.map(person => <p key={person.name} style={{ margin: 0, padding: 0 }}>{person.name} {person.number}</p>)}
+      <div>debug: {newName} {number}</div>
 
     </div>
   )
